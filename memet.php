765648eeb10edea1ed4100de137bac67e9f296e4
acidopal <acidnaufal@gmail.com>
2016-10-17 21:31:49 +0700

    fix conflict

--- memet.php
+++ memet.php
@@ -1,10 +1,6 @@
 <?php
-<<<<<<< HEAD
-for e::echo 'mari belajar';
-?>
-=======
 echo 'mari belajar';
 echo 'blacklist ok';
 echo 'test conflict';
+echo 'qweasd';
 ?>
->>>>>>> f9546874e7ba9559fb0372d6098ef0f4652983a9
